312a656445af1c2c6feda63602af949a63d3e8fe
themer <themer.saanouni@agence-inspire.com>
2024-01-05 17:59:56 +0100

    finished part1 exercice 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,15 @@
-const Header = (props) => <h1>{props.course}</h1>;
+const Header = (props) => <h1>{props.course.name}</h1>;
 
 const Part = (props) => (
   <p>
-    {props.part} {props.exercises}
+    {props.part.name} {props.part.exercises}
   </p>
 );
 
 const Content = (props) => (
   <div>
     {props.parts.map((part, index) => (
-      <Part key={index} part={part.name} exercises={part.exercises} />
+      <Part key={index} part={part} />
     ))}
   </div>
 );
@@ -22,18 +22,20 @@ const Total = (props) => (
 );
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
